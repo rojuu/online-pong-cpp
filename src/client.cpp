e59aa995e1cbc55639735477b23e90c07b04e637
Roni Juppi <roni.juppi@gmail.com>
2017-05-02 00:21:22 +0300

    Trying enet instead. Initting done. Need to do rest later.

--- src/client.cpp
+++ src/client.cpp
@@ -1,11 +1,14 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <stdarg.h> 
+#include <enet/enet.h>
 #include "SDL.h"
-#include "SDL_net.h"
+//#include "SDL_net.h"
 
 const char* HOST = "127.0.0.1";
 const Uint16 PORT = 8080;
-TCPsocket socket;
+//TCPsocket socket;
+ENetHost * client;
 
 const int SCREEN_WIDTH = 640;
 const int SCREEN_HEIGHT = 480;
@@ -18,15 +21,27 @@ void LogSDLError(const char* message)
 	printf("%s, SDLError: %s\n", message, SDL_GetError());
 }
 
-void LogSDLNetError(const char* message)
+//void LogSDLNetError(const char* message)
+//{
+//	printf("%s, SDLNetError: %s\n", message, SDLNet_GetError());
+//}
+
+void DebugLog(const char* message, ...)
 {
-	printf("%s, SDLNetError: %s\n", message, SDLNet_GetError());
+	va_list v;
+	va_start(v, message);
+	char buffer[1024];
+	vsprintf(buffer, message, v);
+	printf("%s\n", buffer);
+	va_end(v);
 }
 
 void ExitCleanUp()
 {
-	SDLNet_TCP_Close(socket);
-	SDLNet_Quit();
+	//SDLNet_TCP_Close(socket);
+	//SDLNet_Quit();
+	enet_host_destroy(client);
+	enet_deinitialize();
 	SDL_DestroyWindow(window);
 	SDL_Quit();
 }
@@ -35,10 +50,11 @@ int main(int argc, char** argv)
 {
 	atexit(ExitCleanUp);
 
-	char message[1024];
-	int length;
-	IPaddress ip;
+	/*int length;
+	IPaddress ip;*/
 	bool hasConnection = true;
+	
+	float pos = 0.134f;
 
 	//INIT
 	if (SDL_Init(SDL_INIT_VIDEO) == -1)
@@ -47,16 +63,35 @@ int main(int argc, char** argv)
 		return 1;
 	}
 
-	if (SDLNet_Init() == -1)
+	if (enet_initialize() != 0)
+	{
+		fprintf(stderr, "An error occurred while initializing ENet.\n");
+		hasConnection = false;
+	}
+	
+	client = enet_host_create(NULL /* create a client host */,
+		1 /* only allow 1 outgoing connection */,
+		2 /* allow up 2 channels to be used, 0 and 1 */,
+		57600 / 8 /* 56K modem with 56 Kbps downstream bandwidth */,
+		14400 / 8 /* 56K modem with 14 Kbps upstream bandwidth */);
+
+	if (client == NULL)
+	{
+		fprintf(stderr,
+			"An error occurred while trying to create an ENet client host.\n");
+		hasConnection = false;
+	}
+
+	/*if (SDLNet_Init() == -1)
 	{
 		LogSDLError("SDLNet_Init");
-		return 2;
+		hasConnection = false;
 	}
 
 	if (SDLNet_ResolveHost(&ip, HOST, PORT) == -1)
 	{
 		LogSDLNetError("SDLNet_ResolveHost");
-		return 3;
+		hasConnection = false;
 	}
 
 	socket = SDLNet_TCP_Open(&ip);
@@ -64,7 +99,7 @@ int main(int argc, char** argv)
 	{
 		LogSDLNetError("SDLNet_TCP_Open");
 		hasConnection = false;
-	}
+	}*/
 
 	window = SDL_CreateWindow("Pong",
 		SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED,
@@ -73,14 +108,14 @@ int main(int argc, char** argv)
 	if (window == NULL)
 	{
 		LogSDLError("SDL_CreateWindow");
-		return 5;
+		return 2;
 	}
 	
 	SDL_Renderer* renderer = SDL_CreateRenderer(window, -1, SDL_RENDERER_ACCELERATED);
 	if (renderer == NULL)
 	{
 		LogSDLError("SDL_CreateRenderer");
-		return 6;
+		return 3;
 	}
 
 	//INIT VARIABLES
@@ -108,15 +143,22 @@ int main(int argc, char** argv)
 		paddleYs[i] = SCREEN_HEIGHT / 2 - paddleHeight / 2;
 	}
 
-	Uint64 CurrentTime = SDL_GetPerformanceCounter();
-	Uint64 LastTime = 0;
-	double DeltaTime = 0;
+	float CurrentTime = (float)SDL_GetPerformanceCounter() / (float)SDL_GetPerformanceFrequency();
+	float StartTime = CurrentTime;
+	float TimeFromStart = CurrentTime - StartTime;
+	float LastTime = 0;
+	float DeltaTime = 0;
+	float NetworkRate = 66; //How many messages per second
+	float TimeFromLastMessage = 1/NetworkRate;
+
 	//MAIN LOOP
 	while (!quit)
 	{
 		LastTime = CurrentTime;
-		CurrentTime = SDL_GetPerformanceCounter();
-		DeltaTime = (double)((CurrentTime - LastTime) / (double)SDL_GetPerformanceFrequency());
+		CurrentTime = (float)SDL_GetPerformanceCounter() / (float)SDL_GetPerformanceFrequency();
+		TimeFromStart = CurrentTime - StartTime;
+		DeltaTime = (float)(CurrentTime - LastTime);
+		TimeFromLastMessage += DeltaTime;
 
 		while (SDL_PollEvent(&e) != 0)
 		{
@@ -124,7 +166,7 @@ int main(int argc, char** argv)
 			{
 				quit = true;
 			}
-			
+
 			if (e.type == SDL_MOUSEMOTION || e.type == SDL_MOUSEBUTTONDOWN || e.type == SDL_MOUSEBUTTONUP)
 			{
 				int x, y;
@@ -132,6 +174,25 @@ int main(int argc, char** argv)
 				paddleYs[0] = y;
 			}
 		}
+
+		//NETWORK MESSAGE
+		if (hasConnection && TimeFromLastMessage > 1/NetworkRate)
+		{
+			//Do stuff
+		}
+
+		////NETWORK MESSAGE
+		//if (hasConnection && TimeFromLastMessage > 1/NetworkRate)
+		//{
+		//	DebugLog("TimeFromStart: %f", TimeFromStart);
+		//	char* buffer = (char*)malloc(sizeof(float));
+		//	*buffer = (char)TimeFromStart;
+		//	int result = SDLNet_TCP_Send(socket, buffer, sizeof(float));
+		//	if (result < sizeof(float))
+		//	{
+		//		LogSDLNetError("SDLNet_TCP_Send");
+		//	}
+		//}
 
 		//RENDERING
 		{
--- src/server.cpp
+++ src/server.cpp
@@ -1,107 +1,165 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <enet/enet.h>
 #include "SDL.h"
-#include "SDL_net.h"
+//#include "SDL_net.h"
 
 const Uint16 PORT = 8080;
 
-void LogSDLError(const char* message)
-{
-	printf("%s, SDLError: %s\n", message, SDL_GetError());
-}
-
-void LogSDLNetError(const char* message)
-{
-	printf("%s, SDLNetError: %s\n", message, SDLNet_GetError());
-}
+ENetAddress address;
+ENetHost * server;
 
 void ExitCleanUp()
 {
-	SDLNet_Quit();
+	//SDLNet_TCP_Close(client);
+	//SDLNet_TCP_Close(server);
+	//SDLNet_Quit();
+	enet_host_destroy(server);
+	enet_deinitialize();
 	SDL_Quit();
+}
+
+void LogSDLError(const char* message)
+{
+	fprintf(stderr, "%s, SDLError: %s\n", message, SDL_GetError());
 }
 
 int main(int argc, char** argv)
 {
 	atexit(ExitCleanUp);
-
-	IPaddress ip, *remoteip;
-	TCPsocket server, client;
-	char message[1024];
-	int len;
-	Uint32 ipaddr;
-
 	if (SDL_Init(0) == -1)
 	{
 		LogSDLError("SDL_Init");
 		return 1;
 	}
 
-	if (SDLNet_Init() == -1)
+	if (enet_initialize() != 0)
 	{
-		LogSDLError("SDLNet_Init");
+		fprintf(stderr, "An error occurred while initializing ENet.\n");
 		return 2;
 	}
-	
-	if (SDLNet_ResolveHost(&ip, NULL, PORT) == -1)
+
+	/* Bind the server to the default localhost.     */
+	/* A specific host address can be specified by   */
+	/* enet_address_set_host (& address, "x.x.x.x"); */
+	address.host = ENET_HOST_ANY;
+	address.port = PORT;
+	server = enet_host_create(&address /* the address to bind the server host to */,
+		32      /* allow up to 32 clients and/or outgoing connections */,
+		2      /* allow up to 2 channels to be used, 0 and 1 */,
+		0      /* assume any amount of incoming bandwidth */,
+		0      /* assume any amount of outgoing bandwidth */);
+
+	if (server == NULL)
 	{
-		LogSDLNetError("SDLNet_ResolveHost");
+		fprintf(stderr,
+			"An error occurred while trying to create an ENet server host.\n");
 		return 3;
-	}
-
-	server = SDLNet_TCP_Open(&ip);
-	if (!server)
-	{
-		LogSDLNetError("SDLNet_TCP_Open");
-		return 4;
-	}
-
-	bool quit = false;
-	printf("Listening to port *%u\n", PORT);
-	while (!quit)
-	{
-		client = SDLNet_TCP_Accept(server);
-		if (!client)
-		{
-			SDL_Delay(100);
-			continue;
-		}
-
-		remoteip = SDLNet_TCP_GetPeerAddress(client);
-		if (!remoteip)
-		{
-			LogSDLNetError("SDLNet_TCP_GetPeerAddress");
-			continue;
-		}
-
-		ipaddr = SDL_SwapBE32(remoteip->host);
-		printf("Accepted a connection from %d.%d.%d.%d port %hu\n",
-			ipaddr >> 24,
-			(ipaddr >> 16) & 0xff,
-			(ipaddr >> 8) & 0xff,
-			ipaddr & 0xff,
-			remoteip->port);
-
-		while (true)
-		{
-			len = SDLNet_TCP_Recv(client, message, 1024);
-			if (!len)
-			{
-				LogSDLNetError("SDLNet_TCP_Recv");
-				continue;
-			}
-
-			printf("Received: %.*s\n", len, message);
-
-			if (message[0] == 'Q')
-			{
-				printf("Quitting on a Q received\n");
-				SDLNet_TCP_Close(client);
-				quit = true;
-				break;
-			}
-		}
 	}
 
 	return 0;
 }
+
+//void LogSDLNetError(const char* message)
+//{
+//	printf("%s, SDLNetError: %s\n", message, SDLNet_GetError());
+//}
+//
+//TCPsocket server, client;
+//
+
+
+//int main(int argc, char** argv)
+//{
+//	atexit(ExitCleanUp);
+//
+//	IPaddress ip, *remoteip;
+//	int length;
+//	Uint32 ipaddr;
+//
+//	if (SDL_Init(0) == -1)
+//	{
+//		LogSDLError("SDL_Init");
+//		return 1;
+//	}
+//
+//	if (SDLNet_Init() == -1)
+//	{
+//		LogSDLError("SDLNet_Init");
+//		return 2;
+//	}
+//	
+//	if (SDLNet_ResolveHost(&ip, NULL, PORT) == -1)
+//	{
+//		LogSDLNetError("SDLNet_ResolveHost");
+//		return 3;
+//	}
+//
+//	server = SDLNet_TCP_Open(&ip);
+//	if (!server)
+//	{
+//		LogSDLNetError("SDLNet_TCP_Open");
+//		return 4;
+//	}
+//
+//	bool quit = false;
+//	printf("Listening to port *%u\n", PORT);
+//	while (!quit)
+//	{
+//		client = SDLNet_TCP_Accept(server);
+//		if (!client)
+//		{
+//			SDL_Delay(100);
+//			continue;
+//		}
+//
+//		remoteip = SDLNet_TCP_GetPeerAddress(client);
+//		if (!remoteip)
+//		{
+//			LogSDLNetError("SDLNet_TCP_GetPeerAddress");
+//			continue;
+//		}
+//
+//		ipaddr = SDL_SwapBE32(remoteip->host);
+//		printf("Accepted a connection from %d.%d.%d.%d port %hu\n",
+//			ipaddr >> 24,
+//			(ipaddr >> 16) & 0xff,
+//			(ipaddr >> 8) & 0xff,
+//			ipaddr & 0xff,
+//			remoteip->port);
+//
+//		while (true)
+//		{
+//			char* buffer = (char*)malloc(sizeof(float));
+//			length = SDLNet_TCP_Recv(client, buffer, sizeof(float));
+//			if (length <= 0)
+//			{
+//				LogSDLNetError("SDLNet_TCP_Recv");
+//				continue;
+//			}
+//
+//			if (buffer)
+//			{
+//				float* time = (float*)malloc(sizeof(float));
+//				*time = (float)*buffer;
+//				if (time)
+//				{
+//					printf("Message: %f\n", time);
+//				}
+//				free(time);
+//			}
+//
+//#if 0
+//			if (message[0] == 'Q')
+//			{
+//				printf("Quitting on a Q received\n");
+//				SDLNet_TCP_Close(client);
+//				quit = true;
+//				break;
+//			}
+//#endif
+//		}
+//	}
+//
+//	return 0;
+//}
